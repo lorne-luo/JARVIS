362a29c0493a893c49d77ef99b763e76b16ba2d0
Lorne <dev@luotao.net>
2019-10-20 17:19:48 +1100

    feat:sms service and client

--- jarvis/grpc/client.py
+++ jarvis/grpc/client.py
@@ -8,12 +8,30 @@ env = Env()
 env.read_env()
 
 JARVIS_HOST = env.str('JARVIS_HOST', default='localhost')
-JARVIS_PORT = env.int('JARVIS_PORT', default=50051)
+JARVIS_PORT = env.int('JARVIS_PORT', default=54321)
 JARVIS_SERVER = f'{JARVIS_HOST}:{JARVIS_PORT}'
 
 
-def sms_admin(message, app):
+def sms_admin(message, app=None):
     with grpc.insecure_channel(JARVIS_SERVER) as channel:
         stub = sms_pb2_grpc.SMSStub(channel)
         response = stub.SMSAdmin(sms_pb2.AuAdminSMSRequest(message=message, app=app))
-    print(f"SMS client received: {response}")
+        return response.success,response.detail
+
+
+def sms_au(mobile_number, message, app=None):
+    with grpc.insecure_channel(JARVIS_SERVER) as channel:
+        stub = sms_pb2_grpc.SMSStub(channel)
+        response = stub.SMSAu(sms_pb2.AuSMSRequest(mobile_number=mobile_number, message=message, app=app))
+    return response.success, response.detail
+
+
+def sms_aliyun(business_id, mobile_numbers, template_code, template_param, app=None):
+    with grpc.insecure_channel(JARVIS_SERVER) as channel:
+        stub = sms_pb2_grpc.SMSStub(channel)
+        response = stub.SMSAliyun(
+            sms_pb2.AliyunSMSRequest(business_id=business_id,
+                                     mobile_numbers=mobile_numbers,
+                                     template_code=template_code,
+                                     template_param=template_param, app=app))
+    return response.success, response.detail
--- jarvis/grpc/sms/service.py
+++ jarvis/grpc/sms/service.py
@@ -1,20 +1,37 @@
+import logging
+
 from . import sms_pb2
 from . import sms_pb2_grpc
 from ...aliyun.sms.service import send_aliyun_sms
 from ...sms import send_to_admin, send_au_sms
 
+logger = logging.getLogger(__name__)
+
 
 class SMS(sms_pb2_grpc.SMSServicer):
 
     def SMSAdmin(self, request, context):
-        success, detail = send_to_admin(request.message, request.app)
+        try:
+            success, detail = send_to_admin(request.message, request.app)
+        except Exception as ex:
+            logger.error(f'[SMSAdmin] {ex}, message={request.message}')
+            return sms_pb2.AuSMSResponse(success=False, detail=str(ex))
         return sms_pb2.AuSMSResponse(success=success, detail=detail)
 
     def SMSAu(self, request, context):
-        success, detail = send_au_sms(request.mobile_number, request.message, request.app)
+        try:
+            success, detail = send_au_sms(request.mobile_number, request.message, request.app)
+        except Exception as ex:
+            logger.error(f'[SMSAu] {ex}, mobile_number={request.mobile_number}, message={request.message}')
+            return sms_pb2.AuSMSResponse(success=False, detail=str(ex))
         return sms_pb2.AuSMSResponse(success=success, detail=detail)
 
     def SMSAliyun(self, request, context):
-        success, detail = send_aliyun_sms(request.business_id, request.mobile_numbers, request.template_code,
-                                          request.template_param, request.app)
+        try:
+            success, detail = send_aliyun_sms(request.business_id, request.mobile_numbers, request.template_code,
+                                              request.template_param, request.app)
+        except Exception as ex:
+            logger.error(
+                f'[SMSAliyun] {ex}, business_id={request.business_id}, mobile_numbers={request.mobile_numbers}, template_code={request.template_code}, template_param={request.template_param}')
+            return sms_pb2.AliyunSMSResponse(success=False, detail=str(ex))
         return sms_pb2.AliyunSMSResponse(success=success, detail=str(detail))
